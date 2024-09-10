64877707f56293e29dbfdbf050bb20c0fa448aa0
clara daibert <86614787+claradaibert@users.noreply.github.com>
2024-09-10 12:10:41 -0300

    Create character details

--- src/pages/CharacterDetails/index.js
+++ src/pages/CharacterDetails/index.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { useDebounce } from "use-debounce";
 
-function CharacterDetails(){
-    return (
-        <>
-            <div>
-                <h1>Detail</h1>
-            </div>
-        </>
-    )
+// Style import
+import { Container } from "./styles";
+
+// Component import
+import DetailsHeader from "../../components/DetailsHeader";
+import CharacterInfo from "../../components/CharacterInfo";
+import CharacterComics from "../../components/CharacterComics";
+
+// Controller import
+import { getCharacter } from "../../controllers/Characters";
+import { getCharacterComics } from "../../controllers/Comics";
+
+function CharacterDetails() {
+  const location = useLocation().pathname;
+
+  const [search, setSearch] = useState("");
+  const [character, setCharacter] = useState({});
+  const [characterComics, setCharacterComics] = useState([]);
+
+  const [searchValue] = useDebounce(search, 1000);
+  const itemId = location.split("/")?.[2];
+
+  useEffect(() => {
+    const getCharacterInfo = async () => {
+      await getCharacter(itemId).then((data) => setCharacter(data));
+
+      await getCharacterComics(itemId).then((data) => setCharacterComics(data));
+    };
+
+    getCharacterInfo();
+  }, [itemId]);
+
+  return (
+    <Container>
+      <div className="backgroundName">{character?.name?.toUpperCase()}</div>
+      <div className="mainContentContainer">
+        <DetailsHeader search={search} setSearch={setSearch} />
+        <CharacterInfo character={character} />
+        <CharacterComics comics={characterComics} />
+      </div>
+    </Container>
+  );
 }
 
 export default CharacterDetails;
--- src/pages/CharacterDetails/styles.js
+++ src/pages/CharacterDetails/styles.js
@@ -1,1 +1,29 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  padding: 2rem 3rem;
+  background-color: ${({ theme }) => theme.COLORS.CHARACTER.BACKGROUND};
+  min-height: 110vh;
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+
+  .backgroundName {
+    position: fixed;
+    margin-top: 5rem;
+    z-index: 0;
+    max-width: 80vw;
+    text-align: right;
+    right: 1rem;
+    font-size: 200px;
+    color: ${({ theme }) => theme.COLORS.CHARACTER.FONT};
+    font-weight: ${({ theme }) => theme.FONT.WEIGHT.G};
+  }
+
+  .mainContentContainer {
+    z-index: 1000;
+    display: flex;
+    flex-direction: column;
+  }
+`;
