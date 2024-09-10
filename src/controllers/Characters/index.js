4788243a11960f90515f9bfe1b827fd82bcb6293
clara daibert <86614787+claradaibert@users.noreply.github.com>
2024-09-10 12:10:57 -0300

    getCharacter req

--- src/controllers/Characters/index.js
+++ src/controllers/Characters/index.js
@@ -5,8 +5,8 @@ import { reqDefaultParams } from "../../constants";
 
 export const getCharacterList = async (search, page, orderByName) => {
   try {
-    const requestSearchValue = search ? `nameStartsWith=${search}&&` : "";
-    const orderBy = orderByName ? 'name' : 'modified';
+    const requestSearchValue = search ? `nameStartsWith=${search}` : "";
+    const orderBy = orderByName ? "name" : "modified";
     const offset = page * 20;
     const response = await api.get(
       `/characters?${requestSearchValue}&orderBy=${orderBy}&offset=${offset}&${reqDefaultParams}`
@@ -14,11 +14,23 @@ export const getCharacterList = async (search, page, orderByName) => {
 
     const responseObject = {
       list: response?.data?.data?.results,
-      total: response?.data?.data?.count,
+      total: response?.data?.data?.total,
     };
 
     return responseObject;
   } catch (err) {
     toast.error(err?.response?.data?.message ?? "Erro buscando personagens");
+  }
+};
+
+export const getCharacter = async (id) => {
+  try {
+    const response = await api.get(`/characters/${id}?${reqDefaultParams}`);
+
+    return response?.data?.data?.results?.[0];
+  } catch (err) {
+    toast.error(
+      err?.response?.data?.message ?? "Erro buscando dados do personagem"
+    );
   }
 };
